52f2f7140696b7a17671808b7bf85af3c50ed2cf
Danial Munsif <danialmunsif@gmail.com>
2021-05-04 12:07:45 +0500

    new mail added

--- functions/secondForm.js
+++ functions/secondForm.js
@@ -5,7 +5,7 @@ const form2 = require("./form2EmailTemplate");
 exports.handler = async function (event, context) {
   await transporter.sendMail({
     from: '"Danial 👻" <dani.uol12@gmail.com>', // sender address
-    to: "salmanhumayun6@gmail.com, danny@techtonex.com", // list of receivers
+    to: "salmanhumayun6@gmail.com, danny@techtonex.com, zaaqib1@gmail.com", // list of receivers
     subject: "Specter Finance ✔", // Subject line
     html: form2(JSON.parse(event.body)), // html body
   });
@@ -13,8 +13,7 @@ exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      event: JSON.parse(event.body)
+      event: JSON.parse(event.body),
     }),
   };
-
 };
