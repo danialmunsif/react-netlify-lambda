4bfbb110c764a13945ce68001645a19adb6276e2
Danial Munsif <danialmunsif@gmail.com>
2021-05-04 15:53:23 +0500

    update the sandgrid

--- functions/utils.js
+++ functions/utils.js
@@ -7,21 +7,14 @@ const nodemailer = require("nodemailer");
 //     pass: "d070064c76025d",
 //   },
 // });
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.sendgrid.net",
-//   port: 587,
-//   auth: {
-//     user: "apikey",
-//     pass:
-//       "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",
-//   },
-// });
+
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.sendinblue.com",
+  host: "smtp.sendgrid.net",
   port: 587,
   auth: {
-    user: "muhammadzaaqib@gmail.com",
-    pass: "O4xyvZTLqUJfb7zN",
+    user: "apikey",
+    pass:
+      "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",
   },
 });
 module.exports = { transporter };
